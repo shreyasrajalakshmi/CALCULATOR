fc9273ab6a05e00786a7ee69db3de0bbe58f69fa
SHREYAS R <118756366+shreyasrajalakshmi@users.noreply.github.com>
2023-01-22 15:45:10 +0530

    COMPLETED

---
+++ calc.js
@@ -0,0 +1,13 @@
+//displsy items
+function display(num){
+result.value +=num    
+}
+function allClear(){
+    result.value=''
+}
+function output(){
+    result.value=eval(result.value)
+}
+function back(){
+    result.value = result.value.slice(0,-1)
+}
